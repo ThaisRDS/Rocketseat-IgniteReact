4484165d96a7278ecbc6dd1fafd1cb666c8ddf0c
Thais Ribeiro <thaisribeiro.ds@gmail.com>
2022-08-06 19:50:30 -0300

    Estrutura da aplicação: CSS global

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Header } from './components/Header/Header';
+import { Post } from './Post'
+import './global.css'
+
 export function App() {
   return (
-    <h1>Hello Word!</h1>
+    <>
+    <Header />
+      <Post
+        author="Carlos Matos"
+        content="Bla bla bla"
+      />
+      <Post
+        author="Pietro Matos"
+        content="Bla bla bla"
+      />
+      <Post
+        author="Isabelle Matos"
+        content="Bla bla bla"
+      />
+    </>
   );
 }
