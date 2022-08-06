e97c9554129431fdc069f3af376bf6b9367078ec
Thais Ribeiro <thaisribeiro.ds@gmail.com>
2022-08-06 20:23:38 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import { Header } from './components/Header/Header';
-import { Post } from './Post'
+import styles from './App.module.css';
 import './global.css'
+
+import { Post } from './Post'
+import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 export function App() {
   return (
     <>
-    <Header />
-      <Post
-        author="Carlos Matos"
-        content="Bla bla bla"
-      />
-      <Post
-        author="Pietro Matos"
-        content="Bla bla bla"
-      />
-      <Post
-        author="Isabelle Matos"
-        content="Bla bla bla"
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Carlos Matos"
+            content="Bla bla bla"
+          />
+          <Post
+            author="Pietro Matos"
+            content="Bla bla bla"
+          />
+          <Post
+            author="Isabelle Matos"
+            content="Bla bla bla"
+          />
+        </main>
+
+      </div>
+
     </>
   );
 }
