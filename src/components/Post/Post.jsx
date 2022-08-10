c3aa3b94288b75053d0aa93f491341e4627fabcd
Thais Ribeiro <thaisribeiro.ds@gmail.com>
2022-08-10 17:09:57 -0300

    Os motores do React - Estado (useState)

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,10 +1,17 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 import { Avatar } from '../Avatar/Avatar';
 import { Comment } from '../Comment/Comment';
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
+
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
         locale: ptBR,
     })
@@ -13,6 +20,12 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBR,
         addSuffix: true
     })
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, comments.length + 1]);
+    }
 
     return (
         <article className={styles.post}>
@@ -40,18 +53,20 @@ export function Post({ author, publishedAt, content }) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong> Deixe seu feedback</strong>
-                <textarea placeholder='Deixe um comentário'></textarea>
+                
+                <textarea placeholder='Deixe um comentário' />
+
                 <footer>
                     <button type='submit'>Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
         </article>
     );
